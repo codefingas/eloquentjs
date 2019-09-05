234e5013b0728d1268274e5fe153d99b297b1ac3
codefingers <udohezekiel34@yahoo.com>
2019-09-05 07:56:20 +0100

    created the phi function

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,12 @@ function addEntry(events, squirrel) {//function for pushing elements into Journa
     journal.push({events, squirrel});
 };
 
+function phi(table) {// function that computes the φ coefficient from an array
+    return (table[3] * table[0] - table[2] * table[1]) / Math.sqrt(
+            table[2] + table[3] * table[0] + table[1] * table[1] + table[3] * table[0] + table[2]);
+};
+
+
+function tableFor(event, journal) {//
+
+}
