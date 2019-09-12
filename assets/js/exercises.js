8b0c13fcb198b98dd038360276ab3b9e92d4f424
codefingers <udohezekiel34@yahoo.com>
2019-09-12 16:32:13 +0100

    created two ways of adding up array values

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -9,3 +9,27 @@ function range(start, end) {
 };
 
 console.log(range(1, 10));
+
+function sum(...array) {
+    let sum = 0;
+
+    for(let arr of array){
+        sum += arr;
+    }
+
+    return sum;
+}
+
+console.log(sum(...range(1, 10)));
+
+function add(array) {
+    let sum = 0;
+
+   for (let i = 0; i < array.length; i++){
+       sum += array[i];
+   }
+
+   return sum;
+}
+
+console.log(add(range(1, 10)));
