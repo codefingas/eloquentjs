d89c6bbe4ecb5a3b6c6d0d07e03f403b12a92d50
codefingers <udohezekiel34@yahoo.com>
2019-10-18 08:26:56 +0100

    picking up higherorder function

--- assets/js/LanguagesScripts.js
+++ assets/js/LanguagesScripts.js
@@ -1150,7 +1150,45 @@ let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
 
 /**end of function for mapping arrays */
 
+/**function that reduces arrays */
+function reduce(array, combine, start) {
+    let current = start;
+
+    for (let element of array){
+        current = combine(current, element);
+    }
+
+    return current;
+}
+
+// console.log(reduce([1, 2, 3, 4], (a,b) => a + b, 0));
+/**end of function that reduces arrays */
+
 
 /**this function reduces the array twice */
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => {
+        return count + (to - from);
+    }, 0);
+};
 
-/**end of array function */
+console.log(SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b : a;
+}));
+/**reducing array twice*/
+
+/**tryiing the apply methd in functions */
+function noisy(f) {
+    
+    return function (aware, arg, boy) {
+        console.log(arguments.length); 
+        console.log("calling with", arg);
+        var val = f.apply(null, arguments);
+        console.log(arguments.length)
+        console.log("called with ", arg, "-got", val);
+        return val;
+    }
+}
+
+console.log(noisy(Boolean)(0));
+/**end of trying apply method on functions */
